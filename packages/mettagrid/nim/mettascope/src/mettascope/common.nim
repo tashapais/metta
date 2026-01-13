5256678eb288d41fc3305f0609d9c4212b81062d
treeform <125948+treeform@users.noreply.github.com>
2026-01-13 01:21:02 +0000

    Fix Emscripten web build. (#4793)

--- packages/mettagrid/nim/mettascope/src/mettascope/common.nim
+++ packages/mettagrid/nim/mettascope/src/mettascope/common.nim
@@ -3,14 +3,10 @@ import
   boxy, windy, vmath, silky,
   replays
 
-# Compile-time fallback: derive data dir from source file location.
-# This works for standalone Nim builds and atlas generation.
-const defaultDataDir = currentSourcePath().parentDir.parentDir.parentDir / "data"
-var dataDir* = defaultDataDir
+var dataDir* = "packages/mettagrid/nim/mettascope/data"
 
 proc setDataDir*(path: string) =
   ## Set the data directory path. Called from Python bindings with absolute path.
-  ## Overrides the compile-time default for runtime use.
   dataDir = path.normalizePath
 
 type
